6f0394ac6ac7fdb013796c2f235d721527f26ecc
abdullah-mamun369 <maamamun369@gmail.com>
2024-10-20 15:55:26 +0600

    3 done

--- Module/src/index.ts
+++ Module/src/index.ts
@@ -53,7 +53,7 @@ import { log } from "console"
 
 
 
-    console.log(getAddressCity(person1));
+    // console.log(getAddressCity(person1));
 
 
 
@@ -69,16 +69,33 @@ import { log } from "console"
     // Create a type guard function isCat that checks if an object is an instance of a Cat class. If it does, the function says "yes, it's a cat." If it doesn't match, the function says "no, it's not a cat."
 
 
+    class Cat {
+        constructor(public cat: string, public sounds: string,) {
+        }
+    }
+    class Dog {
+        constructor(public dog: string, public sounds: string,) {
+        }
+    }
 
 
 
+    const isCat = (animal: any): animal is Cat => {
+        if (animal instanceof Cat) {
+            console.log("yes, it's a cat.");
+            return true;
+        }
+        else {
+            console.log("no, it's not a cat.")
+            return false;
+        }
+    }
 
 
+    const cat = new Cat("cat", "mew")
+    const dog = new Dog("dog", "bark")
 
-
-
-
-
+    isCat(dog)
 
 
 }
